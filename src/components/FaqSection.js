7df5eb5c0995f617361ba91d4abeeb40838bc54a
nanggi4 <nkhun1006@gmail.com>
2021-10-03 12:11:22 +0900

    faq sections update

---
+++ src/components/FaqSection.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>Any Questions <span>FAQ</span></h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>
+            Occaecat culpa ad Lorem consequat qui labore ad cillum nisi occaecat ipsum.
+          </p>
+          <p>
+            Exercitation consectetur velit aliqua sunt.
+            Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule?</h4>
+        <div className="answer">
+          <p>
+            Occaecat culpa ad Lorem consequat qui labore ad cillum nisi occaecat ipsum.
+          </p>
+          <p>
+            Exercitation consectetur velit aliqua sunt.
+            Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diferrent Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Occaecat culpa ad Lorem consequat qui labore ad cillum nisi occaecat ipsum.
+          </p>
+          <p>
+            Exercitation consectetur velit aliqua sunt.
+            Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
+          </p>
+        </div>
+      </div>            
+      <div className="question">
+        <h4>What Products do you offer.</h4>
+        <div className="answer">
+          <p>
+            Occaecat culpa ad Lorem consequat qui labore ad cillum nisi occaecat ipsum.
+          </p>
+          <p>
+            Exercitation consectetur velit aliqua sunt.
+            Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
+          </p>
+        </div>
+      </div>       
+    </div>
+  );
+};
+
+export default FaqSection;

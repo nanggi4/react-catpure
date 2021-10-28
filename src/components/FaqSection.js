fb166564385400bc0bc9cab8595ab9ba13e0bad7
nanggi4 <nkhun1006@gmail.com>
2021-10-29 00:11:03 +0900

    faq style update

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+import { About } from '../styles';
 
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>Any Questions <span>FAQ</span></h2>
       <div className="question">
         <h4>How Do I Start?</h4>
@@ -15,6 +17,7 @@ const FaqSection = () => {
             Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule?</h4>
@@ -27,6 +30,7 @@ const FaqSection = () => {
             Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Diferrent Payment Methods</h4>
@@ -39,6 +43,7 @@ const FaqSection = () => {
             Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>            
       <div className="question">
         <h4>What Products do you offer.</h4>
@@ -51,9 +56,37 @@ const FaqSection = () => {
             Labore do proident est fugiat ipsum ullamco consectetur non cillum in consequat cupidatat.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>       
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2{
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line{
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question{
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer{
+    padding: 2rem 0rem;
+    p{
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;

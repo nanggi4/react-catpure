c626c29abc444e3d5f970d8fa66427a2ee0af808
nanggi4 <nkhun1006@gmail.com>
2021-10-30 17:24:56 +0900

    route update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 //Import Pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+import Nav from './components/Nav';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />   
+        </Route>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>        
+      </Switch>   
     </div>
   );
 }
